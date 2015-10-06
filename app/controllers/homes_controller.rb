95757798168c8fba56038de9f0c13ebc700972ff
Kelsey Hale <kh.khale@gmail.com>
2015-10-06 18:49:58 -0400

    WIP - randomize words and sentences

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,15 @@
 class HomesController < ApplicationController
   def index
+    @noun = Noun.all
+    @verb = Verb.all
+    @adjective = Adjective.all
+    @preposition = Preposition.all
+    @other = Other.all
+
+    @sentences = [" #{@other.sample.word.capitalize} #{@noun.sample.word} #{@preposition.sample.word} #{@verb.sample.word}.", "The #{@adjective.sample.word} #{@noun.sample.word} #{@verb.sample.word} and #{@verb.sample.word}.", "#{@noun.sample.word.capitalize}s #{@verb.sample.word} but #{@adjective.sample.word} #{@adjective.sample.word} #{@noun.sample.word} #{@verb.sample.word}s.", ""]
+    @sentence1 = @sentences.sample
+    @sentence2 = @sentences.sample
+    @sentence3 = @sentences.sample
+    binding.pry
   end
 end
